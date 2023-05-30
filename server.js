ca62c202ebe065eb5f6fb0bd3be3c4201be81d3d
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2023-05-30 09:48:29 +0700

    tambah

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-const express = require('express');
+
 const url = require('url');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  const queryObject = url.parse(req.url, true).query;
+exports.handler = function(event, context, callback) {
+  const queryObject = url.parse(event.path, true).query;
   const radius = queryObject.radius;
 
   if (radius === undefined) {
-    res.statusCode = 400;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Error: Please provide a radius parameter in your query string.\n');
+    callback(null, {
+      statusCode: 400,
+      body: 'Error: Please provide a radius parameter in your query string.'
+    });
   } else {
     const area = Math.PI * radius ** 2;
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`The area of a circle with radius ${radius} is ${area}.\n`);
+    callback(null, {
+      statusCode: 200,
+      body: `The area of a circle with radius ${radius} is ${area}.`
+    });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+};
